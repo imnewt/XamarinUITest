0924ceae65be95fb5333b90f0e8385bd4a1c276c
imnewt <maitantruc1999@gmail.com>
2021-03-20 09:40:06 +0700

    Add project files.

---
+++ XamarinUITest/XamarinUITest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinUITest.ViewModels;
+
+namespace XamarinUITest.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
